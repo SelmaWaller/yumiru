42a064916befad526ba5d2b57ae613836ad8c130
Selma Waller <selmawaller@gmail.com>
2020-06-25 00:14:13 +0200

    changed anime object

--- src/lib/other-objects.js
+++ src/lib/other-objects.js
@@ -1,10 +1,11 @@
 const OtherObjects = [
   {
-    japName: "アニメ",
-    engName: "Anime",
+    furigana1: "てい 提 — あん 案",
+    japName: "アニメ提案",
+    engName: "Anime Tips",
   },
   {
-      furigana1: "に 日 — ほん 本 — ご 語 — まな 学",
+    furigana1: "に 日 — ほん 本 — ご 語 — まな 学",
     japName: "日本語を学ぶ",
     engName: "Learn Japanese",
   },
