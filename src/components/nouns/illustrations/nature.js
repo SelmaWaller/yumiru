fbac14d3e912a12f15011864d2ed4ecfd574179e
Selma Waller <selmawaller@gmail.com>
2020-08-03 17:42:33 +0200

    drawing changes

--- src/components/nouns/illustrations/nature.js
+++ src/components/nouns/illustrations/nature.js
@@ -38,17 +38,17 @@ const Nature = () => {
         </div>
       </div>
       <div className="nature__two">
-        <div className="tree" title="tree">
-          <div className="tree__top" title="tree"></div>
-          <div className="tree__top" title="tree"></div>
-          <div className="tree__trunk" title="tree"></div>
-        </div>
         <div className="rocks" title="rock">
           <div className="rock" title="rock"></div>
           <div className="rock" title="rock"></div>
           <div className="rock" title="rock"></div>
           <div className="rock" title="rock"></div>
           <div className="rock" title="rock"></div>
+          <div className="tree" title="tree">
+            <div className="tree__top" title="tree"></div>
+            <div className="tree__top" title="tree"></div>
+            <div className="tree__trunk" title="tree"></div>
+          </div>
           <div className="grass" title="grass">
             <div className="straw" title="grass"></div>
             <div className="straw" title="grass"></div>
